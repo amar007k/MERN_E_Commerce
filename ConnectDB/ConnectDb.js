bfc78e477161acf20c23e3b05ab9d273abf41d5f
amar007k <53257499+amar007k@users.noreply.github.com>
2025-02-09 01:15:49 +0530

    MERN E - Commerce API

--- ConnectDB/ConnectDb.js
+++ ConnectDB/ConnectDb.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const ConnectDB = async()=>{
-    const  connect = await mongoose.connect("mongodb://localhost:27017/MERN_ECOM");
+   // const  connect = await mongoose.connect("mongodb://localhost:27017/MERN_ECOM");
+   const connect = await mongoose.connect("mongodb+srv://amar:amar123@cluster0.vqxj6.mongodb.net/",{
+    dbName:"MERN_ECOM"
+   })
     if(connect){
         console.log("Database is connected successfully");
     }else{
